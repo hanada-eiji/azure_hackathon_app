d948d80e374be74ed29ff8936aec591bd8958139
eiji <0106eijihana@gmail.com>
2021-12-04 16:51:14 +0900

    テスト

--- api/createPlane/index.js
+++ api/createPlane/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-  context.res = {
-      body: { text: "Hello from the Azure Functions API" }
-  };
-}
+  context.res.json({
+      text: "Hello from the API"
+  });
+};
